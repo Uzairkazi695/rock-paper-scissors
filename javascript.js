14d48ed8e393cb0e079c74bc047927ef2582e80b
Uzairkazi695 <167708998+Uzairkazi695@users.noreply.github.com>
2024-05-09 16:41:22 +0530

    Added html and CSS for user

--- javascript.js
+++ javascript.js
@@ -24,7 +24,7 @@ function getHumanChoice() {
         }
     }
 }
-getHumanChoice();
+// getHumanChoice();
 
 function checkWinner(playerSelection, computerSelection) {
     if(playerSelection === computerSelection) {
